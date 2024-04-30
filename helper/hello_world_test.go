be544c6af7298b1c58db02d4549ec3551cb9d5af
syauqidamario <syauqi.djohan@student.umn.ac.id>
2024-04-30 17:15:06 +0700

    Assertions in Go-lang unit test.

--- helper/hello_world_test.go
+++ helper/hello_world_test.go
@@ -3,6 +3,9 @@ package helper
 import (
 	"fmt"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 //Error() would automatically the Fail() function to consider the unit test fail.
@@ -23,5 +26,21 @@ func TestHelloWorldFatal(t *testing.T){
 	if result != "Hi Syauqi"{
 		t.Fatal("It's supposed to be Hi Syauqi")
 	}
+	fmt.Println("Executed")
+}
+
+//Assertion is to check for the exact value
+//Testify is a Go-Lang unit testing library for assertion
+func TestHelloWorldAssertion(t *testing.T){
+	result := HelloWorld("Syauqi")
+	assert.Equal(t, "Hi Syauqi", result)
+	fmt.Println("Executed")
+}
+
+//assert would call the Fail() function is the checking fails.
+//require would call the FailNow() function to not continue the unit test
+func TestHelloWorldRequire(t *testing.T){
+	result := HelloWorld("Syauqi")
+	require.Equal(t, "Hi Syauqi", result)
 	fmt.Println("Executed")
 }
