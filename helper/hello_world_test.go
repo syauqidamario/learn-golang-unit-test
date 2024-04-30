d26a1da275d46f6f6eda24c775472f8b982665bc
syauqidamario <syauqi.djohan@student.umn.ac.id>
2024-04-30 17:07:55 +0700

    Cancelling a unit test.

--- helper/hello_world_test.go
+++ helper/hello_world_test.go
@@ -1,11 +1,27 @@
 package helper
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
+//Error() would automatically the Fail() function to consider the unit test fail.
 func TestHelloWorld(t *testing.T) {
 	result := HelloWorld("Syauqi")
 	if result != "Hi Syauqi"{
 		//failed unit test
-		panic("Result is not Hi Syauqi")
+		// panic("Result is not Hi Syauqi")
+		// error using t.Error
+		t.Error("It's supposed to be Hi Syauqi")
 	}
+	fmt.Println("Executed")
+}
+
+//Fatal() would do an error log by calling the FailNow() function to stop the unit test execution
+func TestHelloWorldFatal(t *testing.T){
+	result := HelloWorld("Syauqi")
+	if result != "Hi Syauqi"{
+		t.Fatal("It's supposed to be Hi Syauqi")
+	}
+	fmt.Println("Executed")
 }
